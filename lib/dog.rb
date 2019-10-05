d862c69a5e7df277956d40aa8619fcf4f465d27e
virtual-frame-7789 <mttwright@gmail.com>
2019-10-05 01:20:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require "pry"
+
+class Dog
+  @@all = []
+  attr_reader :name
+  
+  def initialize(name)
+    self.save
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts "#{dog.name}"
+    end
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def save
+    @@all.push(self)
+  end
+  
+  
+  
+end
