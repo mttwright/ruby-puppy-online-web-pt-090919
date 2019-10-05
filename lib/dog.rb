8e4b102344a5a3f24acaa843271b1b725a71cdd9
virtual-frame-7789 <mttwright@gmail.com>
2019-10-05 00:53:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Add your code here
+require "pry"
+
+class Dog
+  @@all = []
+  
+  
+  def initialize
+    @@all.push(self)
+    binding.pry
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
+
+ham = Dog.new
